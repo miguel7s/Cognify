4a897a87816a36a64938070264cf35385846150c
miguel7s <99217563+miguel7s@users.noreply.github.com>
2024-11-12 14:43:04 -0800

    Update criminal.py

--- modelo/criminal.py
+++ modelo/criminal.py
@@ -1,1 +1,7 @@
+class Criminal:
+    def __init__(self, name, crime_type):
+        self.name = name
+        self.crime_type = crime_type
 
+    def __str__(self):
+        return f"Criminal: {self.name}, Crime Type: {self.crime_type}"
